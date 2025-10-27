6b669f10ce72fd539c263cce3d1b888540ee03a7
mohammad ali ebrahimzadeh <mohammad77.me@gmail.com>
2025-10-27 16:31:56 +0330

    fix CityQueryDtoValidator

--- WeatherApp.Core/Validators/CityQueryDtoValidator.cs
+++ WeatherApp.Core/Validators/CityQueryDtoValidator.cs
@@ -9,11 +9,16 @@ public class CityQueryDtoValidator : AbstractValidator<CityQueryDto>
     public CityQueryDtoValidator()
     {
         RuleFor(x => x.CityName)
-            .NotNull().NotEmpty()
-            .WithMessage("A city name is required for the weather query.");
-
-        RuleFor(x => x.CityName)
-            .Must(text => RegexHelper.IsPureEnglishRegex.IsMatch(text!))
-            .WithMessage("The city name must contain only standard English");
+         .Custom((value, context) =>
+         {
+             if (string.IsNullOrWhiteSpace(value))
+             {
+                 context.AddFailure("A city name is required for the weather query.");
+             }
+             else if (!RegexHelper.IsPureEnglishRegex.IsMatch(value))
+             {
+                 context.AddFailure("The city name must contain only standard English");
+             }
+         });
     }
 }
